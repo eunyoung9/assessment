5d59f406b12b22f0d494a88949982428465b8cc4
eunyoung9 <84111536+eunyoung9@users.noreply.github.com>
2021-05-13 17:18:13 +0900

    소수/합성수 판별하기

--- PrimeNum.java
+++ PrimeNum.java
@@ -6,31 +6,29 @@ public class PrimeNum {
 //val의 값이 소수인지 합성수인지 판단하는 프로그램 작성하기 
 //0~val-2만큼 루프 돌리기 
 //만약 val을 2부터 나눠보고 나머지가 0이라면 합성수이므로 for문 탈출
-//나머지가 0이 이니라면 div값을 1씩 더하기
-//만약 div의 값이 check와 똑같다면 소수, 다르다면 합성수 출력하기 
+//나머지가 0이 이니라면 isPrimeNum의 값을 false로 
+//만약 div의 값이 true라면 소수, 아니라면 합성수 출력하기 
 
-
-		 int val = 11;
-		 int div = 2;
-		 int check;
-		 int num = 0;
-		 for(int i =0; i<val-2; i++) {
-			 num = val;
-			 if(0==val%div) {
-				 break;
-			 }
-			 div++;
-		 }//for문의 끝
-		 check = div;
-		 if(check == val) { 
-			 //소수
-			 System.out.println(num + "은(는) 소수입니다. ");
-		 }
-		 else {
-			 //합성수
-			 System.out.println(num + "은(는) 합성수입니다.");
-		 }
-
+		int val = 11;
+		int div = 2;
+		boolean isPrimeNum = true;
+		
+		for(int i = 0; i < val - 2; i++) {
+			if(val % div == 0) {
+				isPrimeNum = false;
+				break;
+			}
+			else {
+				
+			}
+			div++;
+		}
+		if(isPrimeNum == true) {
+			System.out.println(val + "은(는) 소수입니다. ");
+		}
+		else {
+		System.out.println(val + "은(는) 합성수입니다. ");
+		}
 	}
 }
 
